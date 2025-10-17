ddba0008ce27ce6715437d02d2be4edbd49565fc
scarletkc <i@scarletkc.com>
2025-10-17 21:14:09 +0800

    Update trigger condition for special summon effects
    Changed the trigger from EVENT_DRAW to EVENT_TO_HAND and added a condition to only activate when the card is added to hand from the deck in both c10000085.lua and c10000087.lua. This ensures the effect only triggers under the intended circumstances.

--- script/c10000087.lua
+++ script/c10000087.lua
@@ -11,8 +11,9 @@ function s.initial(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetCode(EVENT_DRAW)
+	e1:SetCode(EVENT_TO_HAND)
 	e1:SetCountLimit(1)
+	e1:SetCondition(s.thcon)
 	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
@@ -33,6 +34,11 @@ function s.initial(c)
 	e3:SetCondition(s.retcon)
 	e3:SetOperation(s.retop)
 	c:RegisterEffect(e3)
+end
+
+-- 条件：仅从卡组加入手牌时触发
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_DECK)
 end
 
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
